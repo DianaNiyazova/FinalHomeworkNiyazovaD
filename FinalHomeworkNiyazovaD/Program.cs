d2ecfc59ceaa62ef81a8edfe81248af94556915d
DianaNiyazova <anotherdeadangel@mail.ru>
2022-11-24 16:52:03 +0800

    changed the name of second massive

--- FinalHomeworkNiyazovaD/Program.cs
+++ FinalHomeworkNiyazovaD/Program.cs
@@ -1,12 +1,12 @@
 ﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <= 3 символа.
 
 string[] a = { "OMG", "what", "a", "heck", "is", "going", "on" };
-List<string> termslist = new List<string>();
+List<string> b = new List<string>();
 
 for (int i = 0; i < a.Length; i++)
     if (a[i].Length <= 3)
     {
-        termslist.Add(a[i]);
+        b.Add(a[i]);
     }
 
-Console.WriteLine("[{0}]", string.Join(",", termslist));
+Console.WriteLine("[{0}]", string.Join(",", b));
