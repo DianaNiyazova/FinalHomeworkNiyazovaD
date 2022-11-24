e7fd2baefc6db5bfbe37d37558351ed347a0b1ec
DianaNiyazova <anotherdeadangel@mail.ru>
2022-11-24 15:40:08 +0800

    task

--- FinalHomeworkNiyazovaD/Program.cs
+++ FinalHomeworkNiyazovaD/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <= 3 символа.
+
+string[] a = { "what", "a", "heck", "is", "going", "on" };
+List<string> termslist = new List<string>();
+
+for (int i = 0; i < a.Length; i++)
+    if (a[i].Length <= 3)
+    {
+        Console.WriteLine("[0}", string.Join(",", termslist));
+        termslist.Add(a[i]);
+    }
