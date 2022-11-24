46d00c1eb2987039bfe6aeb4319563d8c5f9e3dd
DianaNiyazova <anotherdeadangel@mail.ru>
2022-11-24 15:42:30 +0800

    task2 added one more word

--- FinalHomeworkNiyazovaD/Program.cs
+++ FinalHomeworkNiyazovaD/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <= 3 символа.
 
-string[] a = { "what", "a", "heck", "is", "going", "on" };
+string[] a = { "OMG", "what", "a", "heck", "is", "going", "on" };
 List<string> termslist = new List<string>();
 
 for (int i = 0; i < a.Length; i++)
@@ -8,5 +8,5 @@ for (int i = 0; i < a.Length; i++)
     {
         termslist.Add(a[i]);
     }
-    
+
 Console.WriteLine("[{0}]", string.Join(",", termslist));
